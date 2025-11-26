16ecb5cc0edbac23fdd9a5754c270116eece1874
elijah <Ph5Yc0Fr34K@gmail.com>
2025-11-27 03:01:37 +0800

    first commit

---
+++ src/Portfolio.jsx
@@ -0,0 +1,68 @@
+﻿
+export default function Main() {
+
+    return (
+        <>
+            <div className="mainHeader">
+                <h1 id="mainHeader"> Elijah's Website </h1>
+                <ul className="nav">
+                    <li className="nav"><a className="nav" href="#introduction"> Introduction </a></li>
+                    <li className="nav"><a className="nav" href="#projects"> Projects </a></li>
+                    <li className="nav"><a className="nav" href="#contacts"> Contacts </a></li>
+                </ul>
+            </div>
+
+ 
+
+            <div className="portfolio">
+                {Introduction()}
+                {PortfolioWork()}
+                {ContactInformation()}
+             </div>
+        </>
+    );
+}
+
+function Introduction() {
+    return (
+        <>
+            <h2 className="info" id = "introduction"> ▶ Introduction </h2>
+            <p className="info">
+                My name is Brodit, Elijah Gabriel Pendon,
+                a fresh graduate from Hong Kong Polytechnic University looking for work.
+                My main interest is in both front-end and back-end web development.
+                I'm also eager to learn new technologies and skills for work
+            </p>
+        </>
+    );
+}
+function PortfolioWork() {
+    return (
+        <>
+            <h2 className="info" id = "projects"> ▶ Projects </h2>
+            <p className="info">
+                <u><b>Lost and Found Website</b></u>
+                <br />
+                An interactive Lost-and-Found website where users can register and login with an account, upload by filling in online forms and view lost or found notices and their details.
+                While the admin can view registered users and notices for lost or found items.
+                Accomplished via HTML, CSS, JavaScript for client-side functions, PHP for server-side functions and MySql for managing databases, which was done with the help of XAMPP.
+                <br />
+                <iframe id="video" src="https://drive.google.com/file/d/1eGm2l8DCn9muT--OhS0Pxa3PKT5jnScZ/preview" width="640" height="480" allow="autoplay"></iframe>
+            </p>
+        </>
+    )
+}
+
+function ContactInformation() {
+    return (
+        <>
+            <h2 className="info" id = "contacts"> ▶ Contacts & Links </h2>
+            <p className="info">
+                Email address: elijah200263@gmail.com 
+                <br />
+                GitHub profile link: <a href="https://github.com/eelijah2"> eelijah2 </a>
+            </p>
+
+        </>
+    )
+}
---
+++ src/PortfolioInteractiveNoUse.jsx
@@ -0,0 +1,105 @@
+﻿import { useState } from 'react';
+
+/* 
+This page is the portfolio page but with working interactive headers
+that expand or collapse on click, displaying body information. 
+
+Ultimately not used because it makes readability more inconvenient 
+as more effort is required from user just to display information
+*/
+export default function Main() {
+
+    const [isIntroToggle, setIntroToggle] = useState(true);
+    const [isEduToggle, setEduToggle] = useState(true);
+    const [isWorkToggle, setWorkToggle] = useState(true);
+    const [isPortfolioToggle, setPortfolioToggle] = useState(true);
+    const [isContactToggle, setContactToggle] = useState(true);
+
+    return (
+        <>
+            <div className="mainHeader">
+            <h1 id="mainHeader"> Elijah's Website </h1>
+            </div>
+
+            <div className="portfolio">
+            
+                <button className={isIntroToggle ? "collapseHeader" : "expandHeader"} onClick={() => setIntroToggle(!isIntroToggle)}> ▶ Introduction </button>
+                <br />
+                {Introduction(isIntroToggle)}
+
+                <button className={isEduToggle ? "collapseHeader" : "expandHeader"} onClick={() => setEduToggle(!isEduToggle)}> ▶ Education </button>
+                <br />
+                {Education(isEduToggle)}
+
+                <button className={isWorkToggle ? "collapseHeader" : "expandHeader"} onClick={() => setWorkToggle(!isWorkToggle)}> ▶ Work Experience </button>
+                <br />
+                {WorkExperience(isWorkToggle)}
+
+                <button className={isPortfolioToggle ? "collapseHeader" : "expandHeader"} onClick={() => setPortfolioToggle(!isPortfolioToggle)}> ▶ Projects </button>
+                <br />
+                {PortfolioWork(isPortfolioToggle)}
+
+                <button className={isContactToggle ? "collapseHeader" : "expandHeader"} onClick={() => setContactToggle(!isContactToggle)}> ▶ Contacts & Links </button>
+                <br />
+                {ContactInformation(isContactToggle)}
+
+             </div>
+        </>
+    );
+}
+
+function Introduction(isIntroToggle) {
+    return (
+        <>
+            <p className={isIntroToggle ? "collapseBody" : "expandBody"}>
+                My name is Brodit, Elijah Gabriel Pendon,
+                a fresh graduate from Hong Kong Polytechnic University looking for work.
+                My main interest is in both front-end and back-end web development.
+                I'm also eager to learn new technologies and skills for work
+            </p>
+        </>
+    );
+}
+
+function Education(isEduToggle) {
+    return (
+        <>
+            <p className={isEduToggle ? "collapseBody" : "expandBody"}>
+            Test
+            </p>
+        </>
+    )
+}
+
+function WorkExperience(isWorkToggle) {
+    return (
+        <>
+            <p className={isWorkToggle ? "collapseBody" : "expandBody"}>
+            Test
+            </p>
+        </>
+    )
+}
+
+function PortfolioWork(isPortfolioToggle) {
+    return (
+        <>
+            <p className={isPortfolioToggle ? "collapseBody" : "expandBody"}>
+            Test
+            </p>
+        </>
+    )
+}
+
+function ContactInformation(isContactToggle) {
+    return (
+        <>
+            <p className={isContactToggle ? "collapseBody" : "expandBody"}>
+                Contact my email address: elijah200263@gmail.com for any business enquiries 
+                <br />
+                Link to my GitHub profile:
+            </p>
+
+        </>
+    )
+}
